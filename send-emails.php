3457df3232482c44ecf7ac076168bf6ea49f4327
Jack <jack.gregory.scallan@gmail.com>
2022-07-31 20:37:17 -0400

    Cleaned send-emails.php

--- send-emails.php
+++ send-emails.php
@@ -1,28 +1,19 @@
 <?php
 
 // Import PHPMailer classes into the global namespace
-// These must be at the top of your script, not inside a function
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-// If necessary, modify the path in the require statement below to refer to the
-// location of your Composer autoload.php file.
+
+// Location of Composer autoload.php file
 require '/home/ubuntu/vendor/autoload.php';
 
-// Replace sender@example.com with your "From" address.
-// This address must be verified with Amazon SES.
-
-// Replace recipient@example.com with a "To" address. If your account
-// is still in the sandbox, this address must be verified.
 
 
 // Specify a configuration set. If you do not want to use a configuration
 // set, comment or remove the next line.
 //$configurationSet = 'ConfigSet';
 
-// If you're using Amazon SES in a region other than US West (Oregon),
-// replace email-smtp.us-west-2.amazonaws.com with the Amazon SES SMTP
-// endpoint in the appropriate region.
 
 //SANTAS INFO
 if (isset($_POST["myData"])) {
@@ -42,7 +33,7 @@ function sendEmail($person, $email, $santaName) {
 	$sender = 'jackwebsite101@gmail.com';
 	$senderName = 'Jack';
 
-	
+
 	$host = 'email-smtp.us-east-1.amazonaws.com';
 	$port = 587;
 	// The subject line of the email
@@ -81,7 +72,7 @@ function sendEmail($person, $email, $santaName) {
     	echo "Email sent!" , PHP_EOL;
 	} catch (phpmailerException $e) {
    	 echo "An error occurred. {$e->errorMessage()}", PHP_EOL; //Catch errors from PHPMailer.
-	 echo "-1"; 
+	 echo "-1";
 	exit();
 	} catch (Exception $e) {
     	echo "Email not sent. {$mail->ErrorInfo}", PHP_EOL; //Catch errors from Amazon SES
@@ -92,7 +83,7 @@ function sendEmail($person, $email, $santaName) {
 
 
 foreach($santas as $pair) {
-	sendEmail($pair[0], $pair[1][1], $pair[1][0]); 
+	sendEmail($pair[0], $pair[1][1], $pair[1][0]);
 }
 
 echo "Emails successfully sent!"
