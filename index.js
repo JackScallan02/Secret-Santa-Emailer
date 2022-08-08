d4a3e25d2b7776f3580d35e122bf4f951cdf7044
Jack Scallan <54595949+JackScallan02@users.noreply.github.com>
2022-08-08 17:26:39 -0400

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-
-var topButton = document.getElementById("next-button-6");
-topButton.addEventListener("click", function() {
-  window.scroll({
-    top: 0,
-    left: 0,
-    behavior: 'smooth'
-  })
-});
